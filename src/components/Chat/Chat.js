0bd103d3fef0cdd039da69c17ccd1efa700965a8
TomasGoldenberg <soytomasgoldenberg@gmail.com>
2020-05-12 19:06:12 -0300

    client ready

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -4,6 +4,9 @@ import "./Chat.css"
 import io from "socket.io-client";
 
 import InfoBar  from "../InfoBar/InfoBar"
+import Input from "../Input/Input"
+import Messages from "../Messages/Messages"
+import TextContainer from "../TextContainer/TextContainer"
 
 let socket; 
 
@@ -12,6 +15,7 @@ const Chat = ({location})=>{
     const ENDPOINT = "localhost:5000"
     const [name , setName]= useState("");
     const [room , setRoom]= useState("");
+    const [users, setUsers] = useState('');
     const [message , setMessage]= useState(""); //each message
     const [messages , setMessages]= useState([]);  //store all messages
 
@@ -27,8 +31,10 @@ const Chat = ({location})=>{
         setName(name)
         setRoom(room)
 
-        socket.emit("join",{name, room},()=>{
-           // alert(error) //this error callback it come from the socket.on("join") in the backend, this same event but at the server side
+        socket.emit("join",{name, room},(error)=>{
+            if(error) {
+                alert(error);
+              } //this error callback it come from the socket.on("join") in the backend, this same event but at the server side
 
         })//this is a socket method to emit a socket from the client side it can have any name , we used join
     
@@ -41,11 +47,15 @@ const Chat = ({location})=>{
 
 
        
-    useEffect( ()=>{         //handle messages
-        socket.on("message",(message)=>{       //the params came from the message event in the server, the admins messages
-            setMessages([...messages , message]) //adding each message to the messages array
-        },[])// only restart this useEffect when the messages array updates
-    } )
+        useEffect(() => {
+            socket.on('message', message => {
+              setMessages(messages => [ ...messages, message ]);
+            });
+            
+            socket.on("roomData", ({ users }) => {
+              setUsers(users);
+            });
+        }, []);
     
        
         const sendMessage = (event)=>{
@@ -65,13 +75,10 @@ const Chat = ({location})=>{
         <div className="outerContainer">
             <div className="container">
                 <InfoBar  room={room}/>
-            <input className="input"
-                type="text" 
-                value={message} 
-                onChange={ (event)=> setMessage(event.target.value)}
-                onKeyPress={ (event) =>event.key === "Enter" ? sendMessage(event) : null }/>
-
+                <Messages messages={messages} name={name}/>
+                <Input message={message} setMessage={setMessage} sendMessage={sendMessage}/>
             </div>
+                <TextContainer users={users}/>
         </div>
     )
 }
