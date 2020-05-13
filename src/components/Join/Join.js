1336e19035d2ee07a64b396702cf70a2bcb4c083
TomasGoldenberg <soytomasgoldenberg@gmail.com>
2020-05-12 23:55:53 -0300

    Join Styles finished

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -2,12 +2,14 @@ import React ,{useState} from "react"
 import {Link} from "react-router-dom"
 import "./Join.css" //Styles
 
+
 const Join = ()=>{
     const [name , setName]= useState("");
     const [room , setRoom]= useState("");
 
     return(
        <div className="joinOuterContainer">
+             
            <div className="joinInnerContainer">
                <h1 className="heading">Join </h1> 
                <div> <input placeholder="Name"className="joinInput" type="text" onChange={ (event)=>{ setName(event.target.value) } } /> </div>
