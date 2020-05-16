e11fb08c9138f9537fc0be62e146a85b9a735d89
TomasGoldenberg <soytomasgoldenberg@gmail.com>
2020-05-15 23:39:57 -0300

    updates

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -16,7 +16,7 @@ const Join = ()=>{
                <div> <input placeholder="Room"className="joinInput mt-20" type="text" onChange={ (event)=>{ setRoom(event.target.value) } } /> </div>
                
                <Link onClick={event=> (!name || !room) ? event.preventDefault() :  null }  to={`/chat?name=${name}&room=${room}`} >
-                    <button className="button mt-20" type="submit">Sign In</button>
+                    <button className="button mt-20" type="submit">Login</button>
                </Link>
 
             </div>
